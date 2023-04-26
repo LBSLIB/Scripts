ff930c54dc8e41d417ad605c3f35d1b36b105bb4
LBSLIB <131692983+LBSLIB@users.noreply.github.com>
2023-04-26 10:46:02 +0100

    Update Remove-Appx_AllUsers.ps1

--- Remove-Appx_AllUsers.ps1
+++ Remove-Appx_AllUsers.ps1
@@ -1,41 +1,6 @@
-#Log Function
-    function Write-LogEntry {
-        param (
-            [parameter(Mandatory = $true)]
-            [ValidateNotNullOrEmpty()]
-            [string]$Value,
-            [parameter(Mandatory = $false)]
-            [ValidateNotNullOrEmpty()]
-            [string]$FileName = "AppXRemoval.log",
-            [switch]$Stamp
-        )
-
-        #Build Log File appending System Date/Time to output
-        $LogFile = Join-Path -Path $env:SystemRoot -ChildPath $("Temp\$FileName")
-        $Time = -join @((Get-Date -Format "HH:mm:ss.fff"), " ", (Get-WmiObject -Class Win32_TimeZone | Select-Object -ExpandProperty Bias))
-        $Date = (Get-Date -Format "dd-MM-yyyy")
-
-        If ($Stamp) {
-            $LogText = "<$($Value)> <time=""$($Time)"" date=""$($Date)"">"
-        }
-        else {
-            $LogText = "$($Value)"   
-        }
-
-        Try {
-            Out-File -InputObject $LogText -Append -NoClobber -Encoding Default -FilePath $LogFile -ErrorAction Stop
-        }
-        Catch [System.Exception] {
-            Write-Warning -Message "Unable to add log entry to $LogFile.log file. Error message at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
-        }
-    }
-
- 
-
-
-    Write-LogEntry -Value "##################################"
-    Write-LogEntry -Stamp -Value "Remove-Appx Started"
-    Write-LogEntry -Value "##################################"
+Out-Host "##################################"
+Out-Host "Remove-Appx Started"
+Out-Host "##################################"
 
 
     $BlackListedApps = "Microsoft.BingNews",
@@ -81,6 +46,6 @@ Foreach($BlackListedApp in $BlackListedApps)
 
          If ([string]::IsNullOrEmpty($AppProvisioningPackageNameReCheck) -and ($RemoveAppx.Online -eq $true))
               {
-                   Write-LogEntry -Value "$($BlackListedApp) removed"
+                   Out-Host "$($BlackListedApp) removed"
               }
     }
